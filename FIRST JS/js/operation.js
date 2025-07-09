86fba5f73f849b40e34345617318265e90cb889c
jofazeat <joftaeza@gmail.com>
2025-07-09 12:56:04 +0800

    Add files via upload

---
+++ FIRST JS/js/operation.js
@@ -0,0 +1,30 @@
+
+function sum(num1,num2){
+const answer = num1 + num2;
+console.log("the sum is", answer);
+
+}
+    
+
+
+function difference(num2, num1){
+    const answer = num2;
+    console.log("the difference is", answer);
+}
+
+
+function quotient(num1, num2){
+   const answer =  num1 / num2;
+   console.log("the quotient is", answer);
+}
+function product(num1, num2){
+    const answer = num1  * num2;
+    console.log("the product is", answer);
+
+
+
+
+}   
+
+
+
